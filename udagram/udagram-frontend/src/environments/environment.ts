36f13edaee1b07b6644e1b66f8278050fc29fb57
Mostafa Tarek <mostafatarek7000@gmail.com>
2022-12-21 17:13:15 +0200

    edited circle CI config.yml

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udacity-env.eba-rqh6rpqq.us-east-1.elasticbeanstalk.com//api/v0'
+  apiHost: 'http://udacity-env.eba-rqh6rpqq.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
